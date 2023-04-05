5477fd0baa44119af93b47b4472e631d4b064726
Ziemowit Pałka <ziemowit.palka@cdrl.local>
2023-04-05 12:58:50 +0200

    karol najnowszy dla counters

--- CountersWebsite/frontend/src/App.vue
+++ CountersWebsite/frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App witam elo po stronie developera karola zmiana ale jak juz django dzialsdsdadsassss ess
+  <HelloWorld msg="Welcome to Yourr lcontuenr site
   "/>
 </template>
 
