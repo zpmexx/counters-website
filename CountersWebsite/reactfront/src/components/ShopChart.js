30cb73965377c2ba30458b6328f907736ce8c8e8
karolpi <karol.piechurap1@gmail.com>
2023-04-07 11:38:23 +0200

    dziala

--- CountersWebsite/reactfront/src/components/ShopChart.js
+++ CountersWebsite/reactfront/src/components/ShopChart.js
@@ -1,13 +1,31 @@
 import React from "react";
-import Chart from 'chart.js/auto';
-import { Chart as ChartJS2 } from "react-chartjs-2";
+import { VictoryLine, VictoryChart, VictoryAxis } from "victory";
 
 const ShopChart = ({ chartData }) => {
   if (!chartData) return null;
 
+  const victoryData = chartData.labels.map((label, index) => ({
+    x: label,
+    y: chartData.datasets[0].data[index],
+  }));
+
   return (
-    <div className="w-2/3">
-      <ChartJS2 type="line" data={chartData} options={{}} />
+    <div className="w-2/3 flex justify-center items-center">
+      <div className="square-chart-container">
+        <VictoryChart>
+          <VictoryAxis />
+          <VictoryAxis dependentAxis />
+          <VictoryLine
+            data={victoryData}
+            style={{
+              data: {
+                stroke: "rgb(75, 192, 192)",
+                strokeWidth: 2,
+              },
+            }}
+          />
+        </VictoryChart>
+      </div>
     </div>
   );
 };
