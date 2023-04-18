091510d7876527047f723a83c738ea4305103831
karolpi <karol.piechurap1@gmail.com>
2023-04-18 12:24:09 +0200

    tak2

---
+++ CountersWebsite/reactfront/src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div className="bg-primary p-4 text-white text-3xl tracking-wider">
+      Liczniki CDRL
+    </div>
+  );
+};
+
+export default Navigation;
